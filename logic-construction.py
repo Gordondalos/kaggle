d683f700b380ce6a2e56d13323220616525f7084
gordondalos <esbondarenko@ticketbox.ru>
2020-11-03 21:50:14 +0600

    Logic operator

---
+++ logic-construction.py
@@ -0,0 +1,16 @@
+
+a = 3
+b = 5
+
+my_str = None
+
+# True or False = пустая строка или ноль или None
+if my_str:
+    print('Больше')
+elif a == b:
+    print('Равны')
+else:
+    print('не знаю что писать')
+
+
+
