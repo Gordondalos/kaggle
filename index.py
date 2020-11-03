2855b99ab050f519907f49e2230cd26d0b91bf18
imacdeoxana <oxana.evseeva@gmail.com>
2020-11-03 16:21:44 +0100

    I added some excercises with data types

--- index.py
+++ index.py
@@ -6,7 +6,13 @@ d = 5.3
 
 res = a + str(b)
 
+
 list1 = ['Один', 'Два', 'Три', True] # список = масиив - Изменяемый Массив, нумируется с нуля
+lista=['Oxana', 'cuñao', '26', 11, 'True']
+print(lista[0:])
+lista[1]='blabla'
+print(lista)
+print(type(lista))
 
 list1[0] = 123
 list1[2] = 222
@@ -14,13 +20,24 @@ print('list1', list1)
 
 list2 = [1,2,3,4,5,6,7,8,9,10]
 list3 = [True, False, False]
+lista1=[9,8,7,6,5,4,3,2,1]
+lista1[7]='Oxana'
+print(lista1)
+x=list2+lista1
+print(x)
+print(x[0::2])
+print(x[0::3])
+print(x[1::2])
+print(x[0:-1])
+print(x[::2])
+
 
 list_sdf = ('Один1', 'Два1', 'Три1', 1) # Кортеж не изменяемый массив
+kortezh=('Caracola', 11, '1979', 'lugar', 'precio')
+print(len(kortezh))
+print(kortezh[4])
 
-
-
-
-# print(list1[0])
+#print(list1[0])
 # print(list2[1])
 
 print(len(list1)) # Выводит длину
@@ -44,6 +61,16 @@ user = {
     }
 }
 
+member={
+    'name': ['Clara', 'Ivan', 'unknown', 'Paul']
+    'status': ['beginner', 'profi', 'blanc']
+    'age': ['16', '23', '35', '40', '41', '52']
+    'Nationality': ['Austria', 'France', 'Russia', 'Spain']
+        'salary': ['cualif1', 'cualif2', 'cualif2']
+        'married': False
+        'times in Hac': 3,6,0,0
+}
+print(member)
 # print(user['bio']['names'])
 # print(user['married'])
 #
