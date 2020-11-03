c7092f9bda51a2b273ecf7e9b3996d6b9eb208ac
gordondalos <esbondarenko@ticketbox.ru>
2020-11-03 21:37:12 +0600

    Типы данных

--- index.py
+++ index.py
@@ -9,33 +9,33 @@ res = a + str(b)
 
 list1 = ['Один', 'Два', 'Три', True] # список = масиив - Изменяемый Массив, нумируется с нуля
 lista=['Oxana', 'cuñao', '26', 11, 'True']
-print(lista[0:])
+# print(lista[0:])
 lista[1]='blabla'
-print(lista)
-print(type(lista))
+# print(lista)
+# print(type(lista))
 
 list1[0] = 123
 list1[2] = 222
-print('list1', list1)
+# print('list1', list1)
 
 list2 = [1,2,3,4,5,6,7,8,9,10]
 list3 = [True, False, False]
 lista1=[9,8,7,6,5,4,3,2,1]
 lista1[7]='Oxana'
-print(lista1)
+# print(lista1)
 x=list2+lista1
-print(x)
-print(x[0::2])
-print(x[0::3])
-print(x[1::2])
-print(x[0:-1])
-print(x[::2])
+# print(x)
+# print(x[0::2])
+# print(x[0::3])
+# print(x[1::2])
+# print(x[0:-1])
+# print(x[::2])
 
 
 list_sdf = ('Один1', 'Два1', 'Три1', 1) # Кортеж не изменяемый массив
 kortezh=('Caracola', 11, '1979', 'lugar', 'precio')
-print(len(kortezh))
-print(kortezh[4])
+# print(len(kortezh))
+# print(kortezh[4])
 
 #print(list1[0])
 # print(list2[1])
@@ -45,7 +45,7 @@ print(len(list1)) # Выводит длину
 # list1[-1] = False
 # print(a[0:3]) # Срез списка / словаря / кортежа / строки
 # print(list_sdf[0:2]) # Срез списка / словаря / кортежа
-print(list2[0:10:3]) # Срез списка / словаря / кортежа срез(от:до:шаг)
+# print(list2[0:10:3]) # Срез списка / словаря / кортежа срез(от:до:шаг)
 
 # Словари они же оbject
 # 'Key': 'Value'
@@ -61,15 +61,16 @@ user = {
     }
 }
 
-member={
-    'name': ['Clara', 'Ivan', 'unknown', 'Paul']
-    'status': ['beginner', 'profi', 'blanc']
-    'age': ['16', '23', '35', '40', '41', '52']
-    'Nationality': ['Austria', 'France', 'Russia', 'Spain']
-        'salary': ['cualif1', 'cualif2', 'cualif2']
-        'married': False
-        'times in Hac': 3,6,0,0
+member = {
+    'name': ['Clara', 'Ivan', 'unknown', 'Paul'],
+    'status': ['beginner', 'profi', 'blanc'],
+    'age': ['16', '23', '35', '40', '41', '52'],
+    'Nationality': ['Austria', 'France', 'Russia', 'Spain'],
+        'salary': ['cualif1', 'cualif2', 'cualif2'],
+        'married': False,
+        'times in Hac': [3,6,0,0,]
 }
+
 print(member)
 # print(user['bio']['names'])
 # print(user['married'])
@@ -78,4 +79,4 @@ print(member)
 
 user['smile'] = True
 user['mayArr'] = list2
-print(user)
+# print(user)
